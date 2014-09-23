8c89323c9970d386adde7be7ee7f0babf6f0e44c
wbt11a <beaty.thomas@gmail.com>
2014-09-23 08:57:12 -0400

    Added timestamp to keep the user from having to on successive camera calls from Phonegap.

--- src/android/NativeCameraLauncher.java
+++ src/android/NativeCameraLauncher.java
@@ -14,7 +14,8 @@
    		limitations under the License.   			
  */
 
-package org.wbt11a.nativecamera;
+//package org.wbt11a.nativecamera;
+package com.wezka.nativecamera;
 
 import java.io.File;
 import java.io.FileNotFoundException;
